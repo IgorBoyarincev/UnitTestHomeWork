1836bca8f250115fd57a23804f131c296366d724
IgorBoyarincev <boyarincev.igor@list.ru>
2022-11-02 17:50:55 +0300

    modified main

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -2,13 +2,13 @@ package ru.netology;
 
 public class Main {
     public static void main(String[] args) {
-        long[] sales={40,15,35,12,4,88,56,77,23,60};
-        ManagerSales sale=new ManagerSales(sales);
-        long max=sale.max();
+        long[] sales = {40, 15, 35, 12, 4, 88, 56, 77, 23, 60};
+        ManagerSales sale = new ManagerSales(sales);
+        long max = sale.max();
         System.out.println(max);
-        long min=sale.min();
+        long min = sale.min();
         System.out.println(min);
-        long cutAverage=sale.gutAverage();
+        long cutAverage = sale.gutAverage();
         System.out.println(cutAverage);
 
 
--- src/main/java/ru/netology/ManagerSales.java
+++ src/main/java/ru/netology/ManagerSales.java
@@ -1,33 +1,37 @@
 package ru.netology;
 
 public class ManagerSales {
-    protected long[]sales;
-    public ManagerSales(long[]sales){
-        this.sales=sales;
+    protected long[] sales;
+
+    public ManagerSales(long[] sales) {
+        this.sales = sales;
     }
-    public long max(){
-        long max=Long.MIN_VALUE;
-        for(long sale:sales){
-            if(max<sale){
-                max=sale;
+
+    public long max() {
+        long max = Long.MIN_VALUE;
+        for (long sale : sales) {
+            if (max < sale) {
+                max = sale;
             }
         }
         return max;
     }
-    public long min(){
-        long min=Long.MAX_VALUE;
-        for(long sale:sales){
-            if(min>sale){
-                min=sale;
+
+    public long min() {
+        long min = Long.MAX_VALUE;
+        for (long sale : sales) {
+            if (min > sale) {
+                min = sale;
             }
         }
         return min;
     }
-    public long gutAverage(){
-        long sum=0;
-        for(long sale:sales){
-            sum+=sale;
+
+    public long gutAverage() {
+        long sum = 0;
+        for (long sale : sales) {
+            sum += sale;
         }
-        return (sum-min()-max())/(sales.length/2);
+        return (sum - min() - max()) / (sales.length / 2);
     }
 }
